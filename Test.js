713993b5046b590cd6b360bafd446041b06fd956
Sun43-3rd <157233621+Sun43-3rd@users.noreply.github.com>
2025-12-05 16:43:51 -0500

    Update Test.js
    added csv and proper download

--- Test.js
+++ Test.js
@@ -41,7 +41,9 @@ const Alphas = {
     };
 
 const subject = document.getElementById('calc_subject')
-const law = document.getElementById("calc_law").selectedOptions[0].value
+const law = document.getElementById("calc_law")
+const filetype = document.getElementById('calc_filetype')
+const download = document.getElementById('calc_download')
 
 const regex_operators = /[\/\(\)\^+\*\-]/g;
 const regex_other = /(?<![.!?;]) /g
@@ -57,7 +59,7 @@ function Translate(){
     const a = string.match(regex_split).filter((x) => x != ' ' && x != '\n')
     console.log(a)
     const b = a.map((x) => (operators !== null && operators.indexOf(x) !== -1 ) || !isNaN(x) ?
-     [' ', x, ' '] : Alphas?.['Latin'][law][Alphas.Latin.Alpha.indexOf(x)]).filter((x) => x !== undefined).flat(2).filter((x) => x != ' ')
+     [' ', x, ' '] : Alphas?.['Latin'][law.selectedOptions[0].value][Alphas.Latin.Alpha.indexOf(x)]).filter((x) => x !== undefined).flat(2).filter((x) => x != ' ')
     
     const equation = string
     const equation_tr = b.join('')
@@ -78,15 +80,20 @@ return [equation, equation_tr, translation_str, Astro(translation)]
 
 }
 
-function downloadJSON(content, filename = 'data.json') {
+function download(type, content, filename = 'data') {
     // Convert the string to a Blob (MIME type for JSON)
-    const blob = new Blob([content], { type: 'application/json' });
+    const blob = new Blob([content], {
+        'json': { type: 'application/json' },
+        'txt' : { type: 'text/plain'},
+        'csv' : { type: 'text/csv'}
+    }.[type] 
+    );
 
     // Create a temporary link element
     const url = URL.createObjectURL(blob);
     const a   = document.createElement('a');
     a.href   = url;
-    a.download = filename;          // Suggested file name
+    a.download = `${filename}.${type}`;          // Suggested file name
     document.body.appendChild(a);   // Required for Firefox
 
     a.click();                      // Programmatically click to start download
@@ -96,50 +103,110 @@ function downloadJSON(content, filename = 'data.json') {
     setTimeout(() => URL.revokeObjectURL(url), 1000);
   }
 
+const ShowFile = 
+{
+
+            'JSON' : (data, name, law) => {
+            
+                
+                const json = 
+                    `{  
+                        "Subject" : "${name}",
+                        "Law" : "${law}",
+                        
+                        "Data" : {
+                            "Equation" : "${data[0]}", 
+                            "Equation_Translated" : "${data[1]}", 
+                            "Translation" : "${data[2]}",  
+                            
+                            "Astrology" : {
+                                "Aries" : "${data[3][0][0]} / ${data[3][2][0]} / ${data[3][3][0]} / ${data[3][4][0]}",
+                                "Taurus" : "${data[3][0][1]} / ${data[3][2][1]} / ${data[3][3][1]} / ${data[3][4][1]}",
+                                "Gemini" : "${data[3][0][2]} / ${data[3][2][2]} / ${data[3][3][2]} / ${data[3][4][2]}",
+                                "Cancer" : "${data[3][0][3]} / ${data[3][2][3]} / ${data[3][3][3]} / ${data[3][4][3]}",
+                                "Leo" : "${data[3][0][4]} / ${data[3][2][4]} / ${data[3][3][4]} / ${data[3][4][4]}",
+                                "Virgo" : "${data[3][0][5]} / ${data[3][2][5]} / ${data[3][3][5]} / ${data[3][4][5]}",
+                                "Libra" : "${data[3][0][6]} / ${data[3][2][6]} / ${data[3][3][6]} / ${data[3][4][6]}",
+                                "Scorpio" : "${data[3][0][7]} / ${data[3][2][7]} / ${data[3][3][7]} / ${data[3][4][7]}",
+                                "Sagittarius" : "${data[3][0][8]} / ${data[3][2][8]} / ${data[3][3][8]} / ${data[3][4][8]}",
+                                "Capricorn" : "${data[3][0][9]} / ${data[3][2][9]} / ${data[3][3][9]} / ${data[3][4][9]}",
+                                "Aquarius" : "${data[3][0][10]} / ${data[3][2][10]} / ${data[3][3][10]} / ${data[3][4][10]}",
+                                "Pisces" : "${data[3][0][11]} / ${data[3][2][11]} / ${data[3][3][11]} / ${data[3][4][11]}"
+        
+                            }
+                        }
+                    }`
+    
+                const screen = document.getElementById("results").children[0].children[0];
+                
+                screen.children[1].innerText = json
+                
+            },
+    
+            'Text' : (data, name, law) => {
+                const text =  `
+                        Subject = ${name},
+                        Law = ${law},
+                        Equation = ${data[0]}
+                        Equation_Translated = ${data[1]} 
+                        Translation = ${data[2]}
+                        
+                        Astrology
+                            Aries = ${data[3][0][0]} / ${data[3][2][0]} / ${data[3][3][0]} / ${data[3][4][0]}
+                            Taurus = ${data[3][0][1]} / ${data[3][2][1]} / ${data[3][3][1]} / ${data[3][4][1]}
+                            Gemini = ${data[3][0][2]} / ${data[3][2][2]} / ${data[3][3][2]} / ${data[3][4][2]}
+                            Cancer = ${data[3][0][3]} / ${data[3][2][3]} / ${data[3][3][3]} / ${data[3][4][3]}
+                            Leo = ${data[3][0][4]} / ${data[3][2][4]} / ${data[3][3][4]} / ${data[3][4][4]}
+                            Virgo" = ${data[3][0][5]} / ${data[3][2][5]} / ${data[3][3][5]} / ${data[3][4][5]}
+                            Libra" = ${data[3][0][6]} / ${data[3][2][6]} / ${data[3][3][6]} / ${data[3][4][6]}
+                            Scorpio = ${data[3][0][7]} / ${data[3][2][7]} / ${data[3][3][7]} / ${data[3][4][7]}
+                            Sagittarius = ${data[3][0][8]} / ${data[3][2][8]} / ${data[3][3][8]} / ${data[3][4][8]}
+                            Capricorn = ${data[3][0][9]} / ${data[3][2][9]} / ${data[3][3][9]} / ${data[3][4][9]}
+                            Aquarius = ${data[3][0][10]} / ${data[3][2][10]} / ${data[3][3][10]} / ${data[3][4][10]}
+                            Pisces = ${data[3][0][11]} / ${data[3][2][11]} / ${data[3][3][11]} / ${data[3][4][11]}
+    
+                        }
+                    }`
+    
+                const screen = document.getElementById("results").children[0].children[0];
+                
+                screen.children[1].innerText = text
+                
+            }
+
+            'CSV' : (data, name, law) => {
+                const text =  `
+                ${name},${law},Equation,Equation_Translated,Translation,Astrology,Aries,Taurus,Gemini,Cancer,Leo,Virgo,Libra,Scorpio,Sagittarius,Capricorn,Aquarius,Pisces\n-${name},-${law},${data[2]},${data[0]},${data[1]},,No_Reduction,${data[3][0].join(',')}\n-${name},-${law},,,,,Reduced_1,${data[3][2].join(',')}\n-${name},-${law},,,,,Reduced_2,${data[3][3].join(',')}\n-${name},-${law},,,,,Reduced_3,${data[3][4].join(',')}
+                `
+    
+                const screen = document.getElementById("results").children[0].children[0];
+                
+                screen.children[1].innerText = text
+            }
+                
+        }
+
 function SetUp(){
+    filetype.addEventListener('change', (event) => {
+        const data = Translate()
+        ShowFile[filetype.selectedOptions[0].label](data, subject.innerText.toString(), law.selectedOptions[0].value)
+    })
+    
+    download.addEventListener('click', (event) => {
+        const sub = subject.innerText.toString().replace(/\s*([+\-*/])\s*/g, '_').replaceAll(regex_other, '_').replaceAll(regex_puncutuation, '_').replaceAll('.', '_')
+        
+        const data = document.getElementById("results").children[0].children[0].children[1].innerText;
+        download(filetype.selectedOptions[0].value, data, `CANT_${sub}`)
+    }) 
+    
     subject.addEventListener('keydown', (event) => {
         if(event.key === 'Enter'){
             event.preventDefault() 
-            const sub = subject.innerText.toString().replace(/\s*([+\-*/])\s*/g, '_').replaceAll(regex_other, '_').replaceAll(regex_puncutuation, '_').replaceAll('.', '_')
             const data = Translate()
-
-
-            const json = 
-                `{ 
-                    "Equation" : ${data[0]}, 
-                    "Equation_Translated" : ${data[1]}, 
-                    "Translation" : ${data[2]} ,  
-                    
-                    "Astro" : {
-                        "Aries" : ${data[3][0][0]} / ${data[3][2][0]} / ${data[3][3][0]} / ${data[3][4][0]},
-                        "Taurus" : ${data[3][0][1]} / ${data[3][2][1]} / ${data[3][3][1]} / ${data[3][4][1]},
-                        "Gemini" : ${data[3][0][2]} / ${data[3][2][2]} / ${data[3][3][2]} / ${data[3][4][2]},
-                        "Cancer" : ${data[3][0][3]} / ${data[3][2][3]} / ${data[3][3][3]} / ${data[3][4][3]},
-                        "Leo" : ${data[3][0][4]} / ${data[3][2][4]} / ${data[3][3][4]} / ${data[3][4][4]},
-                        "Virgo" : ${data[3][0][5]} / ${data[3][2][5]} / ${data[3][3][5]} / ${data[3][4][5]},
-                        "Libra" : ${data[3][0][6]} / ${data[3][2][6]} / ${data[3][3][6]} / ${data[3][4][6]},
-                        "Scorpio" : ${data[3][0][7]} / ${data[3][2][7]} / ${data[3][3][7]} / ${data[3][4][7]},
-                        "Saggitarius" : ${data[3][0][8]} / ${data[3][2][8]} / ${data[3][3][8]} / ${data[3][4][8]},
-                        "Capricorn" : ${data[3][0][9]} / ${data[3][2][9]} / ${data[3][3][9]} / ${data[3][4][9]},
-                        "Aquarius" : ${data[3][0][10]} / ${data[3][2][10]} / ${data[3][3][10]} / ${data[3][4][10]},
-                        "Pisces :  ${data[3][0][11]} / ${data[3][2][11]} / ${data[3][3][11]} / ${data[3][4][11]}
-
-                    }
-                }`
-
-            
-
-            const screen = document.getElementById("results").children[0].children[0];
-            console.log(screen.children)
-            screen.children[1].innerText = json
-
-            downloadJSON(json, `CANT_${sub}`)
+            ShowFile[filetype.selectedOptions[0].label](data, subject.innerText.toString(), law.selectedOptions[0].value)
             
         }
-
-
-    }
-)
+    })
 }
 
 
